616057a3fd4c91bab55ea399a0e862894e737eb6
BlancAlexis <alexis.blanc@etu.univ-lyon1.fr>
2023-09-19 11:41:21 +0200

    add maps fragment on default view

--- app/src/main/java/com/example/manageyourcar/view/activities/HomeActivity.kt
+++ app/src/main/java/com/example/manageyourcar/view/activities/HomeActivity.kt
@@ -1,4 +1,35 @@
 package com.example.manageyourcar.view.activities
 
-class HomeActivity {
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.manageyourcar.R
+import com.example.manageyourcar.view.fragments.MapsCardFragment
+
+class HomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentContainerView,MapsCardFragment(),"Liste")
+            .commit()
+
+    }
+
+    override fun onCreateView(
+        parent: View?,
+        name: String,
+        context: Context,
+        attrs: AttributeSet
+    ): View? {
+        return super.onCreateView(parent, name, context, attrs)
+    }
 }
