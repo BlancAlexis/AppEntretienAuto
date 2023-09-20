b5391c4addf277c50856dbf943a1c6d01aadb9fb
BlancAlexis <alexis.blanc@etu.univ-lyon1.fr>
2023-09-20 18:58:57 +0200

    add null check

--- app/src/main/java/com/example/manageyourcar/view/fragments/addUserFragment.kt
+++ app/src/main/java/com/example/manageyourcar/view/fragments/addUserFragment.kt
@@ -22,7 +22,7 @@ import com.example.manageyourcar.viewmodel.UserViewModel
 class addUserFragment : Fragment() {
 
     private lateinit var binding: FragmentAddUserBinding
-    private val userViewModel : UserViewModel by lazy { ViewModelProvider(this).get(UserViewModel::class.java) }
+    private val userViewModel: UserViewModel by lazy { ViewModelProvider(this).get(UserViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,16 +40,19 @@ class addUserFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         userViewModel.liveDataConnect.observe(viewLifecycleOwner) {
-            if(it){
+            if (it) {
                 //Si le bool reçu est à true alors on lance l'activité suivante
-                startActivity(Intent(activity,HomeActivity::class.java))
+                startActivity(Intent(activity, HomeActivity::class.java))
             }
         }
-        binding.mailUser.setOnFocusChangeListener { view,hasFocus ->
+        binding.mailUser.setOnFocusChangeListener { view, hasFocus ->
             if (!hasFocus) {
-                if(!areTextInputLayoutsValid(binding.nameUser.text.toString(), "ccc" )){
-                     binding.layoutNameUser.setBoxBackgroundColorResource(R.color.red_error)
-                }} }
+                if (!areTextInputLayoutsValid(binding.nameUser.text.toString(), "ccc")) {
+                    binding.layoutNameUser.setBoxBackgroundColorResource(R.color.red_error)
+                    //binding.layoutNameUser.setBoxBackgroundColorResource(R.color.red_error)
+                }
+            }
+        }
         binding.passwordUser
         binding.confPassword
         binding.layoutMailUser
@@ -60,13 +63,22 @@ class addUserFragment : Fragment() {
 
         // Check des entrées dans variable, si oui envoie des infos au VM qui répond via liveData
         binding.buttonConf.setOnClickListener {
-            if(areTextInputLayoutsValid(binding.nameUser.text.toString(), "\"^[a-zA-Z\\\\- ]+\$\"" )){
-                return@setOnClickListener;}
-            areTextInputLayoutsValid(binding.mailUser.text.toString(), "ccc" )
-            areTextInputLayoutsValid(binding.passwordUser.text.toString(), " /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/")
-            if(binding.passwordUser.toString().equals(binding.confPassword.toString()))
-            userViewModel.addNewUser(binding.mailUser.toString(),binding.passwordUser.toString(),binding.nameUser.toString())
+            if (areTextInputLayoutsValid(binding.nameUser.text.toString(), "\"^[a-zA-Z\\\\- ]+\$\"")) {
+                return@setOnClickListener;
             }
+            if (areTextInputLayoutsValid(binding.mailUser.text.toString(), "ccc")) {
+                return@setOnClickListener
+            }
+            if (areTextInputLayoutsValid(binding.passwordUser.text.toString(), "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/")) {
+                return@setOnClickListener
+            }
+            if (binding.passwordUser.toString().equals(binding.confPassword.toString()))
+                userViewModel.addNewUser(
+                    binding.mailUser.toString(),
+                    binding.passwordUser.toString(),
+                    binding.nameUser.toString()
+                )
+        }
 
     }
 
@@ -76,16 +88,15 @@ class addUserFragment : Fragment() {
         }
 
     }
-    fun areTextInputLayoutsValid(text :String, regexPattern :String) : Boolean{
-        if (!text.isNullOrEmpty()){
+
+    fun areTextInputLayoutsValid(text: String, regexPattern: String): Boolean {
+        if (!text.isNullOrEmpty()) {
             return Regex(regexPattern).matches(text)
         }
         return false
     }
 
-    fun firstCheck(){
 
-    }
     // fun sendConfSMS(){ Faire si le temps
 //       val smsManager= activity?.getSystemService(SmsManager::class.java)
 //       smsManager?.sendTextMessage()
