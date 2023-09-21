aed5fd136144230330b6bb8ecf1a0be226bc4f8e
BlancAlexis <alexis.blanc@etu.univ-lyon1.fr>
2023-09-21 12:01:33 +0200

    add androidEntryPoint

---
+++ app/src/main/java/com/example/manageyourcar/view/AppApplication.kt
@@ -0,0 +1,10 @@
+package com.example.manageyourcar.view
+
+import android.app.Application
+
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
