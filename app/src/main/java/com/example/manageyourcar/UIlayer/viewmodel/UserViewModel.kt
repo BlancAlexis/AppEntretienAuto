35fe2a4b7f4aab8fd9228d2f52a0e0a4ea04cbb8
BlancAlexis <alexis.blanc@etu.univ-lyon1.com>
2023-10-19 16:38:01 +0200

    (FIX) correct regression after merge in main

--- app/src/main/java/com/example/manageyourcar/UIlayer/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/manageyourcar/UIlayer/viewmodel/UserViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.manageyourcar.dataApi.util.Ressource
+import com.example.manageyourcar.domainLayer.useCaseRetrofit.GetVehiculeByNetworkImmatUseCase
 import com.example.manageyourcar.domainLayer.useCaseRetrofit.GetVehiculeByNetworkUseCase
 import com.example.manageyourcar.domainLayer.useCaseRoom.AddCarToRoomUseCase
 import com.example.manageyourcar.domainLayer.useCaseRoom.GetCarFromRoomUseCase
@@ -14,7 +15,8 @@ import org.koin.core.component.inject
 class UserViewModel : ViewModel(), KoinComponent {
     val liveDataConnect = MutableLiveData<Boolean>()
     val liveDataIsCarAdd = MutableLiveData<Boolean>()
-    val getVehiculeByNetworkUseCase by inject<GetVehiculeByNetworkUseCase>()
+    val getVehiculeBySivNetworkUseCase by inject<GetVehiculeByNetworkUseCase>()
+    val getVehiculeByImmatNetworkUseCase by inject<GetVehiculeByNetworkImmatUseCase>()
     val addCarToRoomUseCase by inject<AddCarToRoomUseCase>()
     val getCarToRoomUseCase by inject<GetCarFromRoomUseCase>()
 
@@ -24,9 +26,9 @@ class UserViewModel : ViewModel(), KoinComponent {
         liveDataConnect.postValue(true)
     }
 
-    fun addNewCarBySIV(SIV: String) {
+    fun getCarByImmat(immat: String) {
         viewModelScope.launch {
-            getVehiculeByNetworkUseCase.getVehiculeBySiv(SIV).collect { result ->
+            getVehiculeByImmatNetworkUseCase.getVehiculeByImmat(immat).collect{ result ->
                 when (result) {
                     is Ressource.Loading -> {
                         println("load")
@@ -47,29 +49,28 @@ class UserViewModel : ViewModel(), KoinComponent {
         }
     }
 
-//    fun addNewCarByImmat(immat: String) {
-//        viewModelScope.launch {
-//            getVehiculeByImmatUseCase.getVehiculeByImmat("CP-370-YK").collect { result ->
-//                when (result) {
-//                    is Ressource.Loading -> {
-//                        println("load")
-//                    }
-//
-//                    is Ressource.Error -> {
-//                        println("Ressource.Error" + result.message)
-//                        // Faire une classe gestion erreur
-//                    }
-//
-//                    is Ressource.Success -> {
-//                        println("Ressource.Success" + result.data)
-//                        // Requete pour vérif si voiture existe puis enregistrement room
-//
-//                    }
-//                }
-//            }
-//            // Requete pour vérif si voiture existe puis enregistrement room
-//
-//        }
+    fun getCarBySIV(SIV: String) {
+        viewModelScope.launch {
+            getVehiculeBySivNetworkUseCase.getVehiculeBySiv(SIV).collect { result ->
+                when (result) {
+                    is Ressource.Loading -> {
+                        println("load")
+                    }
+
+                    is Ressource.Error -> {
+                        println("Ressource.Error" + result.message)
+                        // Faire une classe gestion erreur
+                    }
+
+                    is Ressource.Success -> {
+                        println("Ressource.Success" + result.data)
+                        // Requete pour vérif si voiture existe puis enregistrement room
+
+                    }
+                }
+            }
+        }
+    }
 
         fun addCarToRoom() {
 //            viewModelScope.launch {
