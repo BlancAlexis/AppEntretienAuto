0d5496b05676627979d401f7db54dca43431ba1e
BlancAlexis <naruto.blanc@gmail.com>
2023-11-17 16:26:12 +0100

    [ADD] firebase archi

--- app/src/main/java/com/example/manageyourcar/dataLayer/di/AppModule.kt
+++ app/src/main/java/com/example/manageyourcar/dataLayer/di/AppModule.kt
@@ -49,6 +49,8 @@ import com.example.manageyourcar.domainLayer.useCaseRoom.user.GetUserFromRoomUse
 import com.example.manageyourcar.domainLayer.useCaseRoom.user.GetUsersFromRoomUseCase
 import com.example.manageyourcar.domainLayer.useCaseRoom.user.UpdateUserToRoomUseCase
 import com.example.manageyourcar.domainLayer.utils.SmsSender
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
 import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.context.loadKoinModules
@@ -69,7 +71,8 @@ private val loadFeature by lazy {
             viewModelModule,
             retrofitModule,
             mappersModule,
-            utils
+            utils,
+            firebaseModule
         )
     )
 }
@@ -78,6 +81,9 @@ val utils = module {
     single{SmsSender}
 }
 
+val firebaseModule = module {
+    single{ Firebase.database}
+}
 val mappersModule = module {
     single{ com.example.manageyourcar.domainLayer.mappers.UserMappers }
 }
