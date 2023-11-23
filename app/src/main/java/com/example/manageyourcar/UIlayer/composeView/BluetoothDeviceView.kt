2af6d0637ebac6651999157f5f0bd4528652893b
BlancAlexis <naruto.blanc@gmail.com>
2023-11-23 13:58:44 +0100

    [ADD] bluetooth socket

--- app/src/main/java/com/example/manageyourcar/UIlayer/composeView/BluetoothDeviceView.kt
+++ app/src/main/java/com/example/manageyourcar/UIlayer/composeView/BluetoothDeviceView.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
@@ -19,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.manageyourcar.UIlayer.composeView.UIState.BluetoothUiState
 import com.example.manageyourcar.UIlayer.composeView.UIState.ServicingUIState
 import com.example.manageyourcar.UIlayer.viewmodel.onBluetoothDeviceEvent
@@ -46,29 +50,27 @@ fun BluetoothDeviceView (
     onEvent: (onBluetoothDeviceEvent) -> Unit = {})
 {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Vos prochain entretiens")
-        Button(onClick = { }) {
-            Text(text = "Ajouter un entretien")
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(color = Color.Blue),
-            horizontalArrangement = Arrangement.End
-        ) {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Filled.Menu, contentDescription = "")
-
+        Row {
+            Button(onClick = { onEvent(onBluetoothDeviceEvent.OnBluetoothDeviceScanClick) }) {
+                Text(text = "Start scan")
+            }
+            Button(onClick = { onEvent(onBluetoothDeviceEvent.OnBluetoothDeviceStopScanClick) }) {
+                Text(text = "End scan")
             }
         }
         LazyColumn(modifier = Modifier.fillMaxWidth()) {
             item {
-                Text(text = "Available devices")            }
-            items(uiState.scannedDevices.size) { item ->
-                BluetoothDeviceItemView(uiState.scannedDevices[item] )
+                Text(text = "Available devices")
+            }
+            itemsIndexed(uiState.scannedDevices) { index, item ->
+                Button(onClick = { onEvent(onBluetoothDeviceEvent.OnBluetoothDeviceClick(item)) }) {
+                BluetoothDeviceItemView(item )}
+                Spacer(modifier = Modifier.height(10.dp))
             }
             item {
                  Text(text = "Paired devices")            }
--- app/src/main/java/com/example/manageyourcar/domainLayer/bluetooth/BluetoothController.kt
+++ app/src/main/java/com/example/manageyourcar/domainLayer/bluetooth/BluetoothController.kt
@@ -14,7 +14,7 @@ interface BluetoothController {
     fun startDiscovery()
     fun stopDiscovery()
 
-    fun startBluetoothServer(): Flow<ConnectionResult>
+    //  fun startBluetoothServer(): Flow<ConnectionResult>
     fun connectToDevice(device: BluetoothDevice): Flow<ConnectionResult>
 
     fun closeConnection()
