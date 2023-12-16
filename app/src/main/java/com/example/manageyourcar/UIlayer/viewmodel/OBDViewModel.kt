791db6b0808c80584ac99f8010c6189a605b57f7
BlancAlexis <naruto.blanc@gmail.com>
2023-12-16 03:03:01 +0100

    wip

--- app/src/main/java/com/example/manageyourcar/UIlayer/viewmodel/OBDViewModel.kt
+++ app/src/main/java/com/example/manageyourcar/UIlayer/viewmodel/OBDViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.manageyourcar.UIlayer.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.manageyourcar.domainLayer.bluetooth.ConnectedBluetoothDevice
 import org.koin.core.component.KoinComponent
 
 class OBDViewModel : ViewModel(), KoinComponent {
+    //tab avec toutes les données ou un  livedata pour chaque donnée
+    val connectedBluetoothDevice = ConnectedBluetoothDevice
 }
