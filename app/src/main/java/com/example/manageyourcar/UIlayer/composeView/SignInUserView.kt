5d9193da54cc3ff9dee29dcbe4fbacbd81629d8b
Imperial-CZ <97458821+Imperial-CZ@users.noreply.github.com>
2023-12-18 17:42:28 +0100

    feat: Refonte UI création compte

--- app/src/main/java/com/example/manageyourcar/UIlayer/composeView/SignInUserView.kt
+++ app/src/main/java/com/example/manageyourcar/UIlayer/composeView/SignInUserView.kt
@@ -1,22 +1,38 @@
 package com.example.manageyourcar.UIlayer.composeView
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.manageyourcar.R
 import com.example.manageyourcar.UIlayer.composeView.UIState.SignInUiState
 import com.example.manageyourcar.UIlayer.composeView.common.CustomDialog
 import com.example.manageyourcar.UIlayer.composeView.common.CustomTextField
@@ -27,97 +43,154 @@ fun SignInUserView(
     uiState: SignInUiState,
     onEvent: (UserSubscriptionEvent) -> Unit = {}
 ) {
+
+    val juraFamily = FontFamily(
+        Font(R.font.jura, FontWeight.Medium)
+    )
+
     if (uiState.onInternetLost) {
         CustomDialog(title = "Internet perdu")
     } else {
-        Column(
+        Box(
             modifier = Modifier
-                .fillMaxHeight()
-                .padding(top = 100.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxSize()
+                .background(color = Color(0, 97, 162))
         ) {
-            CustomTextField(
-                textFieldValue = uiState.userLogin,
-                error = uiState.userLoginError,
-                modifier = Modifier
-                    .fillMaxWidth(0.95f)
-                    .padding(10.dp),
-                label = "Identifiant",
-                readOnly = false,
-                keyboardType = KeyboardType.Text,
-                onValueChange = {
-                    onEvent(UserSubscriptionEvent.OnLoginChanged(it))
-                }
-            )
-
-            CustomTextField(
-                textFieldValue = uiState.userFirstName,
-                error = uiState.userFirstNameError,
-                modifier = Modifier
-                    .fillMaxWidth(0.95f)
-                    .padding(10.dp),
-                label = "Firstname",
-                readOnly = false,
-                keyboardType = KeyboardType.Text,
-                onValueChange = {
-                    onEvent(UserSubscriptionEvent.OnFirstnameChanged(it))
-                }
-            )
-            CustomTextField(
-                textFieldValue = uiState.userLastName,
-                error = uiState.userLastNameError,
-                modifier = Modifier
-                    .fillMaxWidth(0.95f)
-                    .padding(10.dp),
-                label = "Lastname",
-                readOnly = false,
-                keyboardType = KeyboardType.Text,
-                onValueChange = {
-                    onEvent(UserSubscriptionEvent.OnLastNameChanged(it))
-                }
-            )
-            CustomTextField(
-                visualTransformation = PasswordVisualTransformation(),
-                error = uiState.userPasswordError,
-                textFieldValue = uiState.userPassword,
-                modifier = Modifier
-                    .fillMaxWidth(0.95f)
-                    .padding(10.dp),
-                label = "Mot de passe",
-                readOnly = false,
-                keyboardType = KeyboardType.Password,
-                onValueChange = {
-                    onEvent(UserSubscriptionEvent.OnPasswordChanged(it))
-                }
-            )
-            CustomTextField(
-                visualTransformation = PasswordVisualTransformation(),
-                textFieldValue = uiState.userValidatePassword,
-                error = uiState.userValidatePasswordError,
-                modifier = Modifier
-                    .fillMaxWidth(0.95f)
-                    .padding(10.dp),
-                label = "Confirmation du mot de passe",
-                readOnly = false,
-                keyboardType = KeyboardType.Password,
-                onValueChange = {
-                    onEvent(UserSubscriptionEvent.OnConfirmPasswordChanged(it))
-                }
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-            Button(
-                onClick = { onEvent(UserSubscriptionEvent.OnClickSendButton) },
-                modifier = Modifier.fillMaxWidth(0.5f)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
             ) {
-                Text(
-                    text = "Valider",
-                    fontSize = 20.sp
+                Icon(
+                    painter = painterResource(R.drawable.baseline_arrow_back_24),
+                    contentDescription = "",
+                    tint = Color(0, 29, 54),
+                    modifier = Modifier.size(36.dp)
                 )
-            }
-            Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    fontFamily = juraFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    text = "Créer votre compte",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 20.dp),
 
+                    )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 100.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                uiState.userLogin?.let {
+                    CustomTextField(
+                        textFieldValue = it,
+                        modifier = Modifier
+                            .fillMaxWidth(0.95f)
+                            .padding(10.dp)
+                            .background(
+                                color = Color(54, 52, 59),
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                        placeholder = "Identifiant",
+                        readOnly = false,
+                        keyboardType = KeyboardType.Text,
+                        onValueChange = {
+                            onEvent(UserSubscriptionEvent.OnLoginChanged(it))
+                        }
+                    )
+                }
+                uiState?.userFirstName?.let {
+                    CustomTextField(
+                        textFieldValue = it,
+                        modifier = Modifier
+                            .fillMaxWidth(0.95f)
+                            .padding(10.dp)
+                            .background(
+                                color = Color(54, 52, 59),
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                        placeholder = "Firstname",
+                        readOnly = false,
+                        keyboardType = KeyboardType.Text,
+                        onValueChange = {
+                            onEvent(UserSubscriptionEvent.OnFirstnameChanged(it))
+                        }
+                    )
+                }
+                uiState.userLastName?.let {
+                    CustomTextField(
+                        textFieldValue = it,
+                        modifier = Modifier
+                            .fillMaxWidth(0.95f)
+                            .padding(10.dp)
+                            .background(
+                                color = Color(54, 52, 59),
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                        placeholder = "Lastname",
+                        readOnly = false,
+                        keyboardType = KeyboardType.Text,
+                        onValueChange = {
+                            onEvent(UserSubscriptionEvent.OnLastNameChanged(it))
+                        }
+                    )
+                }
+                uiState.userPassword?.let {
+                    CustomTextField(
+                        visualTransformation = PasswordVisualTransformation(),
+                        textFieldValue = it,
+                        modifier = Modifier
+                            .fillMaxWidth(0.95f)
+                            .padding(10.dp)
+                            .background(
+                                color = Color(54, 52, 59),
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                        placeholder = "Mot de passe",
+                        readOnly = false,
+                        keyboardType = KeyboardType.Password,
+                        onValueChange = {
+                            onEvent(UserSubscriptionEvent.OnPasswordChanged(it))
+                        }
+                    )
+                }
+                uiState.userValidatePassword?.let {
+                    CustomTextField(
+                        visualTransformation = PasswordVisualTransformation(),
+                        textFieldValue = it,
+                        modifier = Modifier
+                            .fillMaxWidth(0.95f)
+                            .padding(10.dp)
+                            .background(
+                                color = Color(54, 52, 59),
+                                shape = RoundedCornerShape(6.dp)
+                            ),
+                        placeholder = "Confirmation du mot de passe",
+                        readOnly = false,
+                        keyboardType = KeyboardType.Password,
+                        onValueChange = {
+                            onEvent(UserSubscriptionEvent.OnValidatePasswordChanged(it))
+                        }
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = { onEvent(UserSubscriptionEvent.OnClickSendButton) },
+                    modifier = Modifier.fillMaxWidth(0.5f)
+                ) {
+                    Text(
+                        text = "Valider",
+                        fontSize = 20.sp
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+
+            }
         }
     }
 }
