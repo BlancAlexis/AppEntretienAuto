e4808820d41bd33664c7e96ca77c9b016a900a2c
BLANC Alexis <alexis.blanc@etu.univ-lyon1.com>
2024-02-18 15:49:15 +0100

    clean

--- app/src/main/java/com/example/manageyourcar/UIlayer/viewmodel/AddCarViewModel.kt
+++ app/src/main/java/com/example/manageyourcar/UIlayer/viewmodel/AddCarViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.manageyourcar.UIlayer.viewmodel
 
-import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
