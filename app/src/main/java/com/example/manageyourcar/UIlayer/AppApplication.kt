9e24f2d88424fffd284aab988cd8318955973a75
BlancAlexis <blanc.alexispro@gmail.com>
2024-03-05 22:04:44 +0100

    feat(buildVariant): check build variant start

--- app/src/main/java/com/example/manageyourcar/UIlayer/AppApplication.kt
+++ app/src/main/java/com/example/manageyourcar/UIlayer/AppApplication.kt
@@ -1,12 +1,13 @@
 package com.example.manageyourcar.UIlayer
 
 import android.app.Application
+import com.example.manageyourcar.BuildConfig
 import com.example.manageyourcar.dataLayer.di.injectFeature
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.component.KoinComponent
 import org.koin.core.context.GlobalContext.startKoin
 
-class AppApplication : Application(), KoinComponent {
+class AppApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -14,9 +15,13 @@ class AppApplication : Application(), KoinComponent {
             androidContext(this@AppApplication)
             injectFeature()
         }
+
+        // Test buildVariant
+        when (BuildConfig.FLAVOR) {
+            "dev" -> println("dev")
+            "recette" -> println("recette")
+        }
     }
-
-
     companion object {
 
     }
